b9f5d265299ab9c82d847f46bbf9ee9adc08553c
JagadishBotz <154411534+JagadishBotz@users.noreply.github.com>
2023-12-21 21:05:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Souravmkv/AutoFilerAdv/tree/main.git /Souravmkv/AutoFilerAdv 
+  git clone https://github.com/JagadishBotz/Auto-filter.git /Souravmkv/AutoFilerAdv 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Souravmkv/AutoFilerAdv 
+  git clone $UPSTREAM_REPO /JagadishBotz/Auto-filter 
 fi
 cd /THALAPATHY-FILTER-BOT 
 pip3 install -U -r requirements.txt
