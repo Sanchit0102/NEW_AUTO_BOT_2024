599e106ae2ec55f043f6efac2209e92e326fe133
JAGDISH MEENA <154411534+JagadishBotz@users.noreply.github.com>
2023-12-22 14:27:39 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -237,13 +237,15 @@ Rᴇᴘʟʏ Tᴏ Iᴍᴀɢᴇ Aɴᴅ Sᴇɴᴅ /kang Cᴏᴍᴍᴀɴᴅ
 """
 
     IMDB_TEMPLATE_TXT = """
-<b>Query: {query}
-IMDb Data:
+<b>Query: {qurey}
 
+IMDb Data:
 🏷 Title: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
+⏱️ Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10</b>"""
+
     
     ALL_FILTERS = """
 <b>Hᴇʏ {}, Tʜᴇsᴇ ᴀʀᴇ ᴍʏ ᴛʜʀᴇᴇ ᴛʏᴘᴇs ᴏғ ғɪʟᴛᴇʀs.</b>"""
