272c70557997f7168a406e0288ddc256ca73d29b
Abhibotssz <155344413+Abhibotssz@users.noreply.github.com>
2024-01-01 11:21:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/JagadishBotz/Auto-filter.git /Souravmkv/AutoFilerAdv 
+  git clone https://github.com/Abhibotssz/filter.git /Souravmkv/AutoFilerAdv 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /JagadishBotz/Auto-filter 
+  git clone $UPSTREAM_REPO /Abhibotssz/filter 
 fi
 cd /Auto-filter 
 pip3 install -U -r requirements.txt
