cd11c7954941afaf02b816bffd8f50e5796807f1
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-05-16 09:58:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1723,7 +1723,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.DSBOT_TEXT,
+            text=script.DSBOT_TEXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
