35b46ca3542dcf871f835135d3df98b41919e15f
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-06-17 15:01:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'f12bdd3c5c118fdc7ab6dd26bbf360968923f379')
+SHORTLINK_API = environ.get('SHORTLINK_API', '38199a431d38b95e28b2a33062b46dc71ff6bca1')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
