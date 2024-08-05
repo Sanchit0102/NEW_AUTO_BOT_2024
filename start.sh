8f72fda3e683edd605c58133092d043c0377b8c8
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-05 17:18:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-
-if [ -z "$UPSTREAM_REPO" ]; then
+if [ -z $UPSTREAM_REPO ]
+then
   echo "Cloning main Repository"
-  git clone https://github.com/Sanchit0102/NEW_AUTO_BOT_2024.git /Sanchit0102/NEW_AUTO_BOT_2024
+  git clone https://github.com/Sanchit0102/NEW_AUTO_BOT_2024.git /NEW_AUTO_BOT_2024
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO"
-  git clone "$UPSTREAM_REPO" /Sanchit0102/NEW_AUTO_BOT_2024
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /NEW_AUTO_BOT_2024
 fi
-
-cd /Sanchit0102/NEW_AUTO_BOT_2024 || exit
+cd /NEW_AUTO_BOT_2024
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
-
