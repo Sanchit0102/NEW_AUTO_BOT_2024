0f1851403460933d775755a8f3919a069f4c5f12
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-05 22:59:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,39 +101,6 @@ class Bot(Client):
                 yield message
                 current += 1
 
-# =============================[ PORT UPTIME ISSUE FIXED ]================================#
-
-
-RENDER_EXTERNAL_URL = environ.get("RENDER_EXTERNAL_URL", "http://localhost:5000")
-
-def ping_self():
-    url = f"{RENDER_EXTERNAL_URL}/alive"
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            logging.info("Ping successful!")
-        else:
-            logging.error(f"Ping failed with status code {response.status_code}")
-    except Exception as e:
-        logging.error(f"Ping failed with exception: {e}")
-
-def start_scheduler():
-    scheduler = BackgroundScheduler(timezone=pytz.utc)
-    scheduler.add_job(ping_self, 'interval', minutes=3)
-    scheduler.start()
-
-app = Flask(__name__)
-
-@app.route('/alive')
-def alive():
-    return "I am alive!"
-
-def run_flask():
-    app.run(host='0.0.0.0', port=10000)
-
-Thread(target=run_flask).start()
-start_scheduler()
-
 
 
 app = Bot()
